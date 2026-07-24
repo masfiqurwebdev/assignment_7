b86f47b47d8f508a4e6426f63885c0ff94fbb5a9
masfiqurwebdev <masfiq14shovon@gmail.com>
2026-07-24 20:47:46 +0600

    projects restored

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,6 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-        port: "",
-        pathname: "**",
-
-        search: "",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
